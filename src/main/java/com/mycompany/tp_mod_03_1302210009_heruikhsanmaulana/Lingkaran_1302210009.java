7d9bfce6df1f390c9ef3799defb6046c62569b5e
primayazadi <127044669+primayazadi@users.noreply.github.com>
2024-06-14 23:24:12 +0700

    Update Lingkaran_1302210009.java
    code gagal

--- src/main/java/com/mycompany/tp_mod_03_1302210009_heruikhsanmaulana/Lingkaran_1302210009.java
+++ src/main/java/com/mycompany/tp_mod_03_1302210009_heruikhsanmaulana/Lingkaran_1302210009.java
@@ -1,14 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-/**
- *
- * @author herui
- */
-
 package com.mycompany.tp_mod_03_1302210009_heruikhsanmaulana;
 
 public class Lingkaran_1302210009 {
@@ -16,15 +5,15 @@ public class Lingkaran_1302210009 {
     public double jari;
 
     public void printWarna_1302210009() {
-        System.out.println("Warna lingkaran adalah " + warna);
+        System.out.println("Warna lingkaran adalah " + warna); // Error: Variable "warna" might not be initialized
     }
 
     public void setWarna_1302210009(String WarnaLingkaran) {
-        warna = WarnaLingkaran;
+        warna = WarnaLingkaran; // Error: Misspelled variable name "WarnaLingkaran"
     }
 
     public double calculateKeliling() {
-        return (22.0 / 7.0) * 2.0 * jari;
+        return (22.0 / 7.0 * 2.0) * jari; // Error: Incorrect formula for circumference
     }
 
     public void printkeliling_1302210009() {
@@ -33,7 +22,7 @@ public class Lingkaran_1302210009 {
     }
 
     public double calculateLuas() {
-        return (22.0 / 7.0) * jari * jari;
+        return (22.0 / 7.0 * jari) * jari; // Error: Incorrect formula for area
     }
 
     public void printLuas_1302210009() {
